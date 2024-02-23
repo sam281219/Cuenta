015de6f9d676497a50b688c0d73b363a035ca06f
sam281219 <samusic314@gmail.com>
2024-02-23 09:26:40 +0100

    Test

---
+++ probando/Ejercicio/src/test/java/h/ejercicio/EjercicioTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package h.ejercicio;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author samus
+ */
+public class EjercicioTest {
+    
+    public EjercicioTest() {
+    }
+    
+    @Test
+    public void cadena(){
+        Ejercicio ins = new Ejercicio();
+        String cadena = "hola";
+        String resul = ins.getMessage();
+        assertEquals(cadena,resul);
+    }
+
+    
+    
+    
+}
